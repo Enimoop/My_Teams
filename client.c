58c953c0f35919386bd647074d5760691099238f
MForcioli <marineforcioli@gmail.com>
2024-02-25 14:31:48 +0100

    Server et Client

--- client.c
+++ client.c
@@ -1,82 +1,148 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include<arpa/inet.h>
+#include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <curses.h>
 
+#define MESSAGE_WINDOW_HEIGHT 20
+#define MESSAGE_WINDOW_WIDTH 80
+#define INPUT_WINDOW_HEIGHT 3
+#define INPUT_WINDOW_WIDTH 80
+#define HEADER_WINDOW_HEIGHT 1
+#define HEADER_WINDOW_WIDTH 80
 
-int main(int argc, char* argv[]){
-    if(argc==3)
-    {
-        char* ip = argv[1];
-        int port = atoi(argv[2]);
-        char e = '\n';
-        int socket_desc;
-        char message[100];
-        char buffer[1024];
-        char connection[100];
+void init_curses() {
+    initscr(); // Initialise l'environnement curses
+    cbreak(); // Permet la saisie en mode ligne
+    //noecho(); // Désactive l'écho des caractères saisis
+    keypad(stdscr, TRUE); // Permet l'utilisation des touches spéciales
+}
 
-        
-       
-            
-
-        struct sockaddr_in server;
-
-        socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-
-        if (socket_desc == -1) 
-        {
-            printf("Could not create socket");
-        }
-
-        server.sin_addr.s_addr = inet_addr(ip);
-        server.sin_family = AF_INET;
-        server.sin_port = htons(port);
-
-        if (connect(socket_desc, (struct sockaddr * )&server, sizeof(server)) < 0)
-        {
-            puts("connect error");
-            return 1;
-        } else {
-            ssize_t bytes_received = recv(socket_desc, connection, sizeof(connection), 0);
-            printf("%s\n", connection);
-        }
-
-        ùputs("Connected");
-        while(1) {
-            
-            printf("Enter the message: \n");
-            scanf("%s", message);
-            
-            
-            send_message(socket_desc, message);
-
-            receive_message(socket_desc, buffer);
-            
-
-            
-        }
-        return 0;
-    } else {
-        printf("Erreur: nombre d'arguments incorrect\n");
-    }
+void end_curses() {
+    endwin(); // Termine l'environnement curses
 }
 
 void send_message(int socket_desc, char* message) {
-    if (send(socket_desc, message, strlen(message), 0) < 0)
-    {
-        puts("Send failed\n");
-        return;
+    if (send(socket_desc, message, strlen(message), 0) < 0) {
+        printf("Send failed\n");
     }
 }
 
-void receive_message(int socket_desc, char* buffer) {
+void receive_message(int socket_desc, WINDOW *message_win) {
+    char buffer[1024];
     memset(buffer, '\0', sizeof(buffer));
     ssize_t message_recu = recv(socket_desc, buffer, sizeof(buffer), 0);
-    if (message_recu > 0)
-    {
-        printf("Message recu: ");
-        printf("%s\n", buffer);
+    if (message_recu <= 0) {
+        printf("Receive failed\n");
+
+        if (message_recu == 0) {
+            close(socket_desc);
+            end_curses();
+            printf("Server disconnected\n");
+            
+            exit(1);
+        }
+    } else {
+        
+        wprintw(message_win, "%s\n", buffer);
+        wrefresh(message_win);
     }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        printf("Erreur: nombre d'arguments incorrect\n");
+        return 1;
+    }
+
+    char* ip = argv[1];
+    int port = atoi(argv[2]);
+    int socket_desc;
+    char message[1024];
+    char connection[1024];
+    char* pseudo = argv[3];
+    WINDOW *message_win, *input_win, *header_win;
+    
+    fd_set read_fds;
+    struct timeval timeout;
+
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1) {
+        printf("Could not create socket");
+        return 1;
+    }
+
+    struct sockaddr_in server;
+    server.sin_addr.s_addr = inet_addr(ip);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+
+    init_curses();
+
+    // Création des fenêtres
+    message_win = newwin(MESSAGE_WINDOW_HEIGHT, MESSAGE_WINDOW_WIDTH, 0, 0);
+    header_win = newwin(HEADER_WINDOW_HEIGHT, HEADER_WINDOW_WIDTH, MESSAGE_WINDOW_HEIGHT, 0);
+    input_win = newwin(INPUT_WINDOW_HEIGHT, INPUT_WINDOW_WIDTH, MESSAGE_WINDOW_HEIGHT + HEADER_WINDOW_HEIGHT, 0);
+
+    if (connect(socket_desc, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        printf("connect error\n");
+        return 1;
+    } else {
+        int status = send(socket_desc, pseudo, strlen(pseudo), 0);
+        if (status < 0) {
+            printf("Send failed\n");
+        }
+        ssize_t bytes_received = recv(socket_desc, connection, sizeof(connection), 0);
+        wprintw(header_win, "%s\n", "Entrez votre message:");
+        wrefresh(header_win);
+        wprintw(message_win, "%s\n", connection);
+        wrefresh(message_win);
+        memset(connection, '\0', sizeof(connection));
+        
+    }
+
+    
+
+    
+
+    
+
+    scrollok(message_win, TRUE); // Active le défilement automatique dans la fenêtre des messages
+
+    
+    while(1) {
+        FD_ZERO(&read_fds);
+        FD_SET(STDIN_FILENO, &read_fds); 
+        FD_SET(socket_desc, &read_fds); 
+
+        timeout.tv_sec = 2;
+        timeout.tv_usec = 0;
+
+        int activity = select(socket_desc + 1, &read_fds, NULL, NULL, &timeout);
+        if (activity < 0) {
+            printf("Select error\n");
+            return 1;
+        }
+
+        if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+            
+            werase(input_win);
+            mvwgetstr(input_win, 1, 1, message);
+            send_message(socket_desc, message);
+            werase(input_win);
+            wrefresh(input_win);
+        }
+
+        if (FD_ISSET(socket_desc, &read_fds)) {
+            
+            receive_message(socket_desc, message_win);
+        }
+    }
+
+    end_curses();
+    return 0;
 }
--- server.c
+++ server.c
@@ -7,22 +7,42 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define PORT 4242 // le port de notre serveur
 
+// Structure pour représenter chaque client
+typedef struct {
+    int socket_fd;
+    char pseudo[256];
+} Client;
+
+int server_socket;
+
 int create_server_socket(void);
-void accept_new_connection(int listener_socket, fd_set *all_sockets, int *fd_max);
-void read_data_from_socket(int socket, fd_set *all_sockets, int fd_max, int server_socket);
+void accept_new_connection(int listener_socket,fd_set *all_sockets,Client clients[], int *num_clients, int *fd_max);
+void read_data_from_socket(int socket,fd_set *all_sockets, Client clients[], int *num_clients, int server_socket);
+void handle_server_interrupt(int signal);
 
 int main(void)
 {
-    int server_socket;
+    //int server_socket;
     int status;
     int i; // Pour notre bloucle de vérification des sockets
     fd_set all_sockets; // Ensemble de toutes les sockets du serveur
     fd_set read_fds; // Ensemble temporaire pour select()
     int fd_max; // Descripteur de la plus grande socket
     struct timeval timer;
+    Client clients[FD_SETSIZE]; // Tableau pour stocker les informations de chaque client
+    int num_clients = 0;
+
+    // Initialisation du tableau des clients
+    for (i = 0; i < FD_SETSIZE; i++) {
+        clients[i].socket_fd = -1; // Initialise tous les descripteurs de fichiers à -1 (non connecté)
+        memset(clients[i].pseudo, '\0', sizeof(clients[i].pseudo)); // Initialise tous les pseudos à une chaîne vide
+    }
+
+
     // Création de la socket du serveur
     server_socket = create_server_socket();
     if (server_socket == -1)
@@ -42,6 +62,7 @@ int main(void)
     FD_SET(server_socket, &all_sockets); // Ajout de la socket principale à l'ensemble
     fd_max = server_socket; // Le descripteur le plus grand est forcément celui de notre seule socket
     printf("[Server] Set up select fd sets\n");
+
     while (1) // Boucle principale
     {
         // Copie l'ensemble des sockets puisque select() modifie l'ensemble surveillé
@@ -77,12 +98,13 @@ int main(void)
             // La socket est prête à être lue !
             if (i == server_socket)
             // La socket est notre socket serveur qui écoute le port
-                accept_new_connection(server_socket, &all_sockets, &fd_max);
+                accept_new_connection(server_socket,&all_sockets, clients, &num_clients, &fd_max);
             else
             // La socket est une socket client, on va la lire
-                read_data_from_socket(i, &all_sockets, fd_max, server_socket);
+                read_data_from_socket(i,&all_sockets, clients, &num_clients, server_socket);
             i++;
         }
+        signal(SIGINT, handle_server_interrupt);
     }
         return (0);
 }
@@ -94,6 +116,7 @@ int create_server_socket(void)
     struct sockaddr_in sa;
     int socket_fd;
     int status;
+    int enable_reuse = 1; // Définir l'option SO_REUSEADDR
     // Préparaton de l'adresse et du port pour la socket de notre serveur
     memset(&sa, 0, sizeof sa);
     sa.sin_family = AF_INET; // IPv4
@@ -108,6 +131,13 @@ int create_server_socket(void)
         return (-1);
     }
     printf("[Server] Created server socket fd: %d\n", socket_fd);
+
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &enable_reuse, sizeof(enable_reuse)) < 0) {
+        printf("[Server] setsockopt(SO_REUSEADDR) failed: %s\n", strerror(errno));
+        close(socket_fd);
+        return (-1);
+    }
+
     // Liaison de la socket à l'adresse et au port
     status = bind(socket_fd, (struct sockaddr *)&sa, sizeof sa);
     if (status != 0)
@@ -121,65 +151,131 @@ int create_server_socket(void)
 
 
 // Accepte une nouvelle connexion et ajoute la nouvelle socket à l'ensemble des sockets
-void accept_new_connection(int server_socket, fd_set *all_sockets, int *fd_max)
+void accept_new_connection(int server_socket,fd_set *all_sockets, Client clients[], int *num_clients, int *fd_max)
 {
     int client_fd;
-    char msg_to_send[BUFSIZ];
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    char msg_to_send[BUFSIZ+256];
     int status;
-    client_fd = accept(server_socket, NULL, NULL);
+
+    client_fd = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
     if (client_fd == -1)
     {
         printf("[Server] Accept error: %s\n", strerror(errno));
         return;
     }
+
     FD_SET(client_fd, all_sockets); // Ajoute la socket client à l'ensemble
+
     if (client_fd > *fd_max)
         *fd_max = client_fd; // Met à jour la plus grande socket
+
+
+    char pseudo[256];
+    ssize_t bytes_received = recv(client_fd, pseudo, sizeof(pseudo), 0);
+    if (bytes_received <= 0) {
+        printf("[Server] Receive error for client %d: %s\n", client_fd, strerror(errno));
+        close(client_fd);
+        FD_CLR(client_fd, all_sockets);
+        return;
+    }
+    
+
+
+
+    // Ajoute le nouveau client à la liste des clients
+    strncpy(clients[*num_clients].pseudo, pseudo, sizeof(clients[*num_clients].pseudo));
+    clients[*num_clients].socket_fd = client_fd;
+    (*num_clients)++;
+
     printf("[Server] Accepted new connection on client socket %d.\n", client_fd);
+
     memset(&msg_to_send, '\0', sizeof msg_to_send);
-    sprintf(msg_to_send, "Welcome. You are client fd [%d]\n", client_fd);
+    sprintf(msg_to_send, "Welcome %s. You are client fd [%d]\n",pseudo, client_fd);
+    printf("msg_to_send: %s\n", msg_to_send);
     status = send(client_fd, msg_to_send, strlen(msg_to_send), 0);
     memset(&msg_to_send, '\0', sizeof msg_to_send);
+     memset(&pseudo, '\0', sizeof pseudo);
     if (status == -1)
         printf("[Server] Send error to client %d: %s\n", client_fd, strerror(errno));
 }
 
 // Lit le message d'une socket et relaie le message à toutes les autres
-void read_data_from_socket(int socket, fd_set *all_sockets, int fd_max, int server_socket)
+void read_data_from_socket(int socket,fd_set *all_sockets, Client clients[], int *num_clients, int server_socket)
 {
     char buffer[BUFSIZ];
-    char msg_to_send[BUFSIZ];
+    char msg_to_send[BUFSIZ+300];
     int bytes_read;
     int status;
+    int i;
+
     memset(&buffer, '\0', sizeof buffer);
     bytes_read = recv(socket, buffer, BUFSIZ, 0);
     if (bytes_read <= 0)
     {
         if (bytes_read == 0)
             printf("[%d] Client socket closed connection.\n", socket);
+
+            
         else
             printf("[Server] Recv error: %s\n", strerror(errno));
         close(socket); // Ferme la socket
         FD_CLR(socket, all_sockets); // Enlève la socket de l'ensemble
-    }
-    else
-        {
+
+
+
+
+        // Enlève le client de la liste des clients
+        for (i = 0; i < *num_clients; i++) {
+            if (clients[i].socket_fd == socket) {
+                // Décale les autres clients vers la gauche pour combler le trou
+                for (int j = i; j < *num_clients - 1; j++) {
+                    clients[j] = clients[j + 1];
+                }
+                break;
+            }
+        }
+        (*num_clients)--;
+
+
+
+    } else {
         printf("[%d] Got message: %s\n", socket, buffer);
         // Relaie le message à toutes les autres sockets
-        for (int i = 0; i <= fd_max; i++)
+        char pseudo[256];
+        for (i = 0; i < *num_clients; i++) {
+            if (clients[i].socket_fd == socket) {
+                strncpy(pseudo, clients[i].pseudo, sizeof(pseudo));
+                break;
+            }
+        }
+
+        for (i = 0; i <= *num_clients - 1; i++)
         {
-            
-            if (FD_ISSET(i, all_sockets) && i != server_socket)
+            printf("Client %d\n", i);
+            if (clients[i].socket_fd != server_socket)
             {
-                //memset(&msg_to_send, '\0', sizeof msg_to_send);
-                //sprintf(msg_to_send, "[%d] %s", socket, buffer);
-                printf("buffer : %s\n", buffer);
-                status = send(i, buffer, strlen(buffer), 0);
-                memset(&buffer, '\0', sizeof(buffer));
+                printf("Sending message to client %s\n", clients[i].pseudo);
+                memset(&msg_to_send, '\0', sizeof(msg_to_send));
+                sprintf(msg_to_send, "[%s] %s", pseudo, buffer);
+                status = send(clients[i].socket_fd, msg_to_send, strlen(msg_to_send), 0);
+
                 printf("status: %d\n", status);
                 if (status == -1)
                     printf("[Server] Send error to client %d: %s\n", i, strerror(errno));
+                    
             }
         }
-    } 
-}
+        memset(&pseudo, '\0', sizeof pseudo);
+        memset(&buffer, '\0', sizeof buffer);
+        memset(&msg_to_send, '\0', sizeof msg_to_send);
+    }
+        
+} 
+
+void handle_server_interrupt(int signal)
+{
+    close(server_socket);
+    exit(0);
+} 
